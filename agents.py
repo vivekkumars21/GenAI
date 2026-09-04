00df4b04bec295f2b09d4d495221d8b3a5109c6b
Knights24 <vivekvishwakarma21@outlook.com>
2026-09-04 19:26:11 +0530

    Add chatbot model initialization and prompt formatting

--- agents.py
+++ agents.py
@@ -1,3 +1,15 @@
 from dotenv import load_dotenv
 
 load_dotenv()
+
+from langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+model = ChatMistralAI(model_name="mistral-small-2506")
+parse = StrOutputParser()
+
+short_temp = ChatPromptTemplate.from_template("Explain {topic} in short.")
+
+formatted_prompt = short_temp.format_prompt(topic="data science")
+response = model.invoke(formatted_prompt)
